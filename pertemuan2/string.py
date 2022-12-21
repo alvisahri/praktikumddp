0ebd04c50e7fd03f067ce31f2b1536fc759329bb
alvisahri <114494103+alvisahri@users.noreply.github.com>
2022-12-21 19:37:59 +0700

    Add files via upload

---
+++ pertemuan2/string.py
@@ -0,0 +1,6 @@
+s = "Hello World!"
+
+print(s[1:-1:2])
+print(s.lower())                     #mencetak hello
+print(s.upper())                     #mencetak HELLO
+print(s.replace('World', 'Alvi'))    #mencetak Hello Alvi 
---
+++ pertemuan2/while.py
@@ -0,0 +1,20 @@
+# angka = 1
+
+
+# while angka > 0 and angka < 11 :
+#     angka = int(input("Masukan bilangan bulat"))
+#     if angka >= 1:
+#         print("Bilangan Positif")
+#     elif angka < 0: 
+#         print("Bilangan Negatif")
+
+while True:
+    nama = input("Masukan nama Anda: ")
+
+    if nama =="x":
+        break 
+
+    elif nama == "Adi":
+        continue
+    
+    print("Selamat datang", nama) 
